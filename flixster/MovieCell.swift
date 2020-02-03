d2ecf05f5d4defeeea2d134a31b21f4a053358af
Kharee MacElree <kbmacelree17@students.desu.edu@Kharees-MacBook-Pro.local>
2020-02-02 20:23:59 -0500

    Initial Commit

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Kharee MacElree on 2/2/20.
+//  Copyright © 2020 Kharee MacElree. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
