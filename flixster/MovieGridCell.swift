ab08ad00a21062f0c604cf4540915d02856db7cb
Kharee <khareemacelree@yahoo.com>
2020-02-07 02:03:40 -0500

    Completed the entire project, added tab bar and super hero grid

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Kharee MacElree on 2/7/20.
+//  Copyright © 2020 Kharee MacElree. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
